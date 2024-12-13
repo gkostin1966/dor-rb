772426d95f730ec10a691acc383cffad90e2844a
Greg Kostin <gkostin@umich.edu>
2024-12-13 13:04:07 -0500

    feature file with pending steps

---
+++ features/step_definitions/store_resource.rb
@@ -0,0 +1,11 @@
+Given("a package containing the scanned pages, OCR, and metadata") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the Collection Manager places the packaged resource in the incoming location") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the Collection Manager can see that it was preserved.") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
