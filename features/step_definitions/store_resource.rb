891c52fe9e1c5ac50b535d2de5b09b83aacd708a
Greg Kostin <gkostin@umich.edu>
2024-12-13 13:45:17 -0500

    first steps

--- features/step_definitions/store_resource.rb
+++ features/step_definitions/store_resource.rb
@@ -1,11 +1,44 @@
+class Package
+  def entities
+    []
+  end
+end
+
+class Inbox
+  def push(package)
+  end
+end
+
+class Storage
+  def has_entities(entities)
+    false
+  end
+end
+
+module WorldProperties
+  def package
+    @package ||= Package.new
+  end
+
+  def inbox
+    @inbox ||= Inbox.new
+  end
+
+  def storage
+    @storage ||= Storage.new
+  end
+end
+
+World(WorldProperties)
+
 Given("a package containing the scanned pages, OCR, and metadata") do
-  pending # Write code here that turns the phrase above into concrete actions
+  package
 end
 
 When("the Collection Manager places the packaged resource in the incoming location") do
-  pending # Write code here that turns the phrase above into concrete actions
+  inbox.push(package)
 end
 
 Then("the Collection Manager can see that it was preserved.") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(storage.has_entities(package.entities)).to be true
 end
